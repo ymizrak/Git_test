c0de7f3282e7f2832e89e0c2b419bd30a74f4ea6
ymizrak <67202737+ymizrak@users.noreply.github.com>
2020-06-22 00:00:40 +0200

    Update FirstClass.java
    ilk commit remote repositoryden

--- eclipse-workspace/git_project001/src/package01/FirstClass.java
+++ eclipse-workspace/git_project001/src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk comit from local repositoryden");
+		System.out.println("ilk comit from remote repositoryden");
 		
 	}
 
